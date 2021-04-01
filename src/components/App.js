62435072f6ec7d9f59d0be26da47c09881a482eb
gordongest <gordon.gest@gmail.com>
2021-04-01 12:34:32 -0600

    adding SongDetail component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import SongList from './SongList';
+import SongDetail from './SongDetail';
 
-const App = (props) => (
+const App = () => (
   <div>
-    {console.log(props)}
-    <SongList />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
+    />
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+        <div className="column eight wide">
+          <SongDetail />
+        </div>
+      </div>
+    </div>
   </div>
 );
 
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,7 +1,48 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectSong } from '../actions'
 
-export default class SongList extends Component {
+class SongList extends Component {
+  mapList() {
+    return this.props.songs.map(song => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+            >
+              select
+            </button>
+          </div>
+          <div className="content">
+            <p>{song.title}</p>
+          </div>
+        </div>
+      )
+    })
+  }
+
   render() {
-    return <div>hola, amigo</div>;
+    console.log(this.props)
+    return (
+      <div className="ui divided list">
+        {this.mapList()}
+      </div>
+    );
   }
 }
+
+/* take the data held in store("state") and map it to a props object */
+const mapStateToProps = (state) => {
+  console.log(state)
+  return {
+    songs: state.songs,
+    selectedSong: state.selectedSong
+  }
+}
+
+/* connect returns a function, so in order to pass state through to the component it is
+required to pass the SongList component as the second function call argument to connect */
+/* to get the action creator wired in, pass it as a second argument to the first call */
+export default connect(mapStateToProps, { selectSong })(SongList);
